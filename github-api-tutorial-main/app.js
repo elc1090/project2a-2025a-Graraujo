bc021ed44d8d033bd973cbe6e59816caa0aa96d9
Giulia Rodrigues de Araujo <tenagi@live.com>
2025-04-13 21:27:06 -0300

    alterações no index, style e app.js
    inserção do mascote, mudanças no index.html, style.css e app.js

--- github-api-tutorial-main/app.js
+++ github-api-tutorial-main/app.js
@@ -3,62 +3,91 @@ const gitHubForm = document.getElementById('gitHubForm');
 
 // Listen for submissions on GitHub username input form
 gitHubForm.addEventListener('submit', (e) => {
-
-    // Prevent default form submission action
     e.preventDefault();
 
     // Get the GitHub username input field on the DOM
     let usernameInput = document.getElementById('usernameInput');
-
-    // Get the value of the GitHub username input field
+    let repositoryInput = document.getElementById('repositoryInput');
+    // Get the value of the GitHub username and repository input fields
     let gitHubUsername = usernameInput.value;
+    let githubRepository = repositoryInput.value;
 
-    // Run GitHub API function, passing in the GitHub username
-    requestUserRepos(gitHubUsername)
-        .then(response => response.json()) // parse response into json
-        .then(data => {
-            // update html with data from github
-            for (let i in data) {
-                // Get the ul with id of userRepos
+    // se não tiver o nome do repositório, então busca todos os repositórios do usuário
+    if (githubRepository === "") {
+        requestUserRepos(gitHubUsername)
+            .then(response => response.json()) // parse response into json
+            .then(data => {
+                // update html with data from github
+                let ul = document.getElementById('userRepos');
+                ul.innerHTML = ''; // limpa resultados anteriores
 
+                // Check if the username was found or not
                 if (data.message === "Not Found") {
-                    let ul = document.getElementById('userRepos');
-
                     // Create variable that will create li's to be added to ul
                     let li = document.createElement('li');
 
                     // Add Bootstrap list item class to each li
-                    li.classList.add('list-group-item')
+                    li.classList.add('list-group-item');
                     // Create the html markup for each li
-                    li.innerHTML = (`
-                <p><strong>No account exists with username:</strong> ${gitHubUsername}</p>`);
+                    li.innerHTML = `
+                        <p><strong>No account exists with username:</strong> ${gitHubUsername}</p>
+                    `;
                     // Append each li to the ul
                     ul.appendChild(li);
                 } else {
-
-                    let ul = document.getElementById('userRepos');
-
-                    // Create variable that will create li's to be added to ul
-                    let li = document.createElement('li');
-
-                    // Add Bootstrap list item class to each li
-                    li.classList.add('list-group-item')
-
-                    // Create the html markup for each li
-                    li.innerHTML = (`
-                <p><strong>Repo:</strong> ${data[i].name}</p>
-                <p><strong>Description:</strong> ${data[i].description}</p>
-                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
-            `);
-
-                    // Append each li to the ul
-                    ul.appendChild(li);
+                    // Show all repositories
+                    for (let i in data) {
+                        // Create variable that will create li's to be added to ul
+                        let li = document.createElement('li');
+                        // Add Bootstrap list item class to each li
+                        li.classList.add('list-group-item');
+                        // Create the html markup for each li
+                        li.innerHTML = `
+                            <p><strong>Repo:</strong> ${data[i].name}</p>
+                            <p><strong>Description:</strong> ${data[i].description}</p>
+                            <p><strong>URL:</strong> <a href="${data[i].html_url}" target="_blank">${data[i].html_url}</a></p>
+                        `;
+                        // Append each li to the ul
+                        ul.appendChild(li);
+                    }
                 }
-            }
-        })
-})
+            });
+    } else {
+        // se não, mostra os commits do repositório passado
+        requestRepoCommits(gitHubUsername, githubRepository)
+            .then(response => response.json()) // parse response into json
+            .then(data => {
+                let ul = document.getElementById('userRepos');
+                ul.innerHTML = ''; // limpa resultados anteriores
+
+                // Check if the repository was found or not
+                if (data.message === "Not Found") {
+                    let li = document.createElement('li');
+                    li.classList.add('list-group-item');
+                    li.innerHTML = `<p><strong>Repository not found:</strong> ${githubRepository}</p>`;
+                    ul.appendChild(li);
+                } else {
+                    // Show commits of the repository
+                    for (let i in data) {
+                        let li = document.createElement('li');
+                        li.classList.add('list-group-item');
+                        li.innerHTML = `
+                            <p><strong>Commit Message:</strong> ${data[i].commit.message}</p>
+                            <p><strong>Date:</strong> ${new Date(data[i].commit.author.date).toLocaleString()}</p>
+                        `;
+                        ul.appendChild(li);
+                    }
+                }
+            });
+    }
+});
 
 function requestUserRepos(username) {
-    // create a variable to hold the `Promise` returned from `fetch`
+    // create a variable to hold the Promise returned from fetch
     return Promise.resolve(fetch(`https://api.github.com/users/${username}/repos`));
+}
+
+function requestRepoCommits(username, repository) {
+    // create a variable to hold the Promise returned from fetch
+    return Promise.resolve(fetch(`https://api.github.com/repos/${username}/${repository}/commits`));
 }
